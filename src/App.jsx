cf4d5f6a858f26129af78dad658bf79018a95018
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-26 20:50:38 +0800

    feat(router): integrate react-router-dom and setup initial routes
    Added BrowserRouter to enable client-side routing.
    Defined the root route ("/") to render the RecipePage component.
    Integrated the Nav component into the main layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Nav } from "src/components/Nav";
+import { RecipePage } from "src/pages/RecipePage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="relative min-w-[375px] bg-teal700">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<RecipePage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
