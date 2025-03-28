c491b54443cba50ef822f92713d66b257ba0963f
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-28 23:31:00 +0800

    style(steps-cards): refine styles and improve hover effects
    - Adjusted hover effects for step cards.
    - Improved background color transitions for better visual feedback.

--- src/components/StepsCards.jsx
+++ src/components/StepsCards.jsx
@@ -1,4 +1,4 @@
-import { MdDoneOutline } from "react-icons/md"; 
+import { MdDoneOutline } from "react-icons/md";
 import { RECIPE_DATA } from "src/recipe.js";
 import { useState } from "react";
 import Slider from "react-slick";
@@ -46,29 +46,26 @@ export function StepsCards() {
   };
   return (
     <div className="mt-6 h-full w-full space-y-6 py-6">
-      <h4 className="font-semi-bold text-start font-chocolateClassicalSans text-[20px] leading-[32px] tracking-[0px] text-yellow400">
+      <h4 className="text-start font-chocolateClassicalSans text-2xl font-semibold leading-[32px] tracking-[0px] text-yellow400">
         Directions
       </h4>
 
       {/* cards */}
-      <div className="h-full w-full p-8 outline outline-blue-500">
-        <Slider
-          {...settings}
-          className="h-full w-full gap-x-5"
-        >
+      <div className="h-full w-full rounded-xl border border-yellow p-8">
+        <Slider {...settings} className="h-full w-full">
           {steps.map((item, index) => {
             return (
-              <div key={index} className="h-full px-2">
+              <div key={index} className="h-full p-4">
                 <div
-                  className={`group relative flex h-full cursor-pointer flex-col items-center space-y-6 rounded-3xl bg-beige p-[30px] before:absolute before:inset-0 before:rounded-3xl before:border-4 hover:before:border-yellow ${stepsStatus[index] ? "before:border-teal" : "before:border-transparent"} cardHoverShadow cardHoverShadow:hover`}
+                  className={`group relative flex h-full cursor-pointer flex-col items-center space-y-6 rounded-3xl bg-beige p-[30px] text-blue800 before:absolute before:inset-0 before:rounded-3xl before:border-4 hover:before:border-yellow200 ${stepsStatus[index] ? "before:border-orange" : "before:border-transparent"} cardHoverShadow cardHoverShadow:hover`}
                   onClick={() => handleClick(index)}
                 >
                   <div className="flex w-full items-center justify-between">
                     <span
-                      className={`text-nowrap rounded-3xl p-1 px-4 text-xl group-hover:bg-yellow ${stepsStatus[index] ? "bg-teal" : "bg-beige300"}`}
+                      className={`text-nowrap rounded-3xl p-1 px-4 text-xl group-hover:bg-yellow400 ${stepsStatus[index] ? "bg-orange" : "bg-yellow"}`}
                     >{`Step ${index + 1}/${steps.length}`}</span>
                     <span
-                      className={`leading-0 text-6xl font-black 500:text-[90px] ${stepsStatus[index] ? "text-teal" : "text-gray-400 opacity-30"} group-hover:text-gray-400 group-hover:opacity-80`}
+                      className={`leading-0 text-6xl font-black 500:text-[90px] ${stepsStatus[index] ? "animate-popIn text-orange" : "text-gray-400 opacity-30"} group-hover:text-orange group-hover:opacity-40`}
                     >
                       <MdDoneOutline />
                     </span>
@@ -84,10 +81,11 @@ export function StepsCards() {
         </Slider>
       </div>
 
-      <p className="text-beige">
+      <p className="text-5xl text-beige">
         {steps.length > 0 ? (
           <span>
-            {((doneStepsNum / steps.length) * 100).toFixed(0)} %
+            {((doneStepsNum / steps.length) * 100).toFixed(0)}
+            %{" "}
           </span>
         ) : (
           <span>0%</span>
