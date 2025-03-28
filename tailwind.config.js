dbb5b4c916d1eb2b1d74382a1ca8ac620d440e6e
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-28 23:42:18 +0800

    feat(tailwind): add new color shades and animations
    - Updated yellow and orange color shades for consistency.
    - Added new blue color variations (blue100 - blue900).
    - Introduced popIn and noiseEffect animations for UI enhancements.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,14 @@ export default {
         white300:"#E8E5DC",
         beige: "#DFCFB4",
         beige300: "#CFB07B",
-        yellow: "#FFDF71",
-        yellow200:"#FCCF0C",
-        yellow400:"#F4B303",
-        orange: "#FD7135",
+        beige500: "#917262",
+        yellow: "#FFFCC9",
+        yellow200:"#FFEF88",
+        yellow400:"#FFC444 ",
+        orange: "#FF841F",
+        orange100: "#FFC89A",
+        orange600: "#9B4A24",
+        orange900: "#62381F",
         pink: "#F79597",
         pink200: "#D9667E",
         red: "#DC4E42",
@@ -22,6 +26,13 @@ export default {
         teal300: "#067B7D",
         teal500: "#01504A",
         teal700: "#27403E",
+        blue100: "#92CEDD",
+        blue200: "#23B5FE",
+        blue300: "#354DFF",
+        blue400: "#2825B4",
+        blue500: "#1B2B8C",
+        blue800: "#13164A",
+        blue900: "#1A1B33"
       },
       fontFamily: {
         chocolateClassicalSans : ["Chocolate Classical Sans", "sans-serif"],
@@ -33,9 +44,24 @@ export default {
         "575": "575px",
         "990": "990px",
         "1440": "1440px"
+      },
+      keyframes : {
+        popIn: {
+          "0%" : { transform: "scale(0.5)", opacity: "0" },
+          "80%" : { transform: "scale(1.2)", opacity: "1" },
+          "100%" : { transform: "scale(1)" }
+        },
+        noiseEffect : {
+          "0%" : {backgroundPosition: "0 0"},
+          "100%" : {backgroundPosition: "100% 100%"}
+        }
+
+      },
+      animation: {
+        popIn: "popIn 1s ease-in-out",
+        noiseEffect: "noiseEffect 1s steps(10) infinite"
       }
     },
   },
   plugins: [],
 }
-
