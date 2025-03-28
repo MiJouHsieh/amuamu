57e3ab396197335f00193c599105c1b6567580a4
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-29 02:19:39 +0800

    style(app): update background color and layout spacing
    - Changed background color from teal700 to blue900.
    - Added flexbox properties for better layout alignment.
    - Adjusted max-width and padding for improved responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import {
 
 export function App() {
   return (
-    <div className="relative min-w-[375px] bg-teal700">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<RecipePage />} />
-        </Routes>
-      </Router>
+    <div className="relative flex min-w-[375px] flex-col items-center bg-blue900">
+      <div className="w-full px-4 pb-14 md:px-8 1440:max-w-[1110px]">
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<RecipePage />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
