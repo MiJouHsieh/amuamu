4ad21f17f739f362773bf59d2a3526535f9dbfdb
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-28 23:35:46 +0800

    style(nav): update background color and spacing
    - Changed navbar background color from teal700 to blue900.
    - Adjusted max-width from 1440px to 1110px for better alignment.
    - Updated icon margin for improved spacing.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 export function Nav() {
   return (
-    <nav className="660:p-0 1440:top-10 bg-teal700 flex h-[88px] w-full max-w-[1440px] items-center justify-between p-6 text-white md:h-24">
+    <nav className="660:p-0 bg-bg-blue900 flex h-[88px] w-full items-center justify-between p-6 text-white md:h-24 1440:top-10 1440:max-w-[1110px]">
       <div className="1440:relative">
         <Link to="/">
-          <IconLogo className="660:mx-10 1440:mx-16 h-10 w-10 md:h-12 md:w-12" />
+          <IconLogo className="660:mx-10 h-10 w-10 md:h-12 md:w-12 1440:mx-16" />
         </Link>
       </div>
     </nav>
