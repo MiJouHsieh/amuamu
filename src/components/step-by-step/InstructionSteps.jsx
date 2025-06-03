9300ff89de3bd05d3d7e94c060280eecc384793e
MiJouHsieh <mijouhsieh@gmail.com>
2025-06-03 17:47:11 +0800

    refactor(instruction-steps): remove unused props and simplify done logic
    - Removed debug logs, unnecessary props (`trackDoneSteps`, `isLastStep`, `index`)
    and replaced conditional button rendering with a single `isAllStepsDone` check.
    - Also removed useSwiperSlide hook as it's no longer needed.

--- src/components/step-by-step/InstructionSteps.jsx
+++ src/components/step-by-step/InstructionSteps.jsx
@@ -1,38 +1,15 @@
 import { CookingStepsCards } from "src/components/step-by-step/CookingStepsCards";
-import { useSwiperSlide } from "swiper/react";
-import { useEffect } from "react";
 
-export function InstructionSteps({
-  recipe,
-  onPrev,
-  onNext,
-  isLastStep,
-  trackDoneSteps,
-  setTrackDoneSteps,
-  index
-}) {
-  
-  const { isActive } = useSwiperSlide();
-  useEffect(() => {
-    if (isActive) {
-      console.log("✅ ACTIVE slide");
-      console.log("trackDoneSteps", trackDoneSteps);
-      console.log("isLastStep", isLastStep);
-      console.log("index", index);
-    }
-  }, [isActive]);
+export function InstructionSteps({ recipe, onPrev, onNext, isAllStepsDone, setIsAllStepsDone }) {
   return (
     <div className="flex w-full flex-col items-center justify-center space-y-4 py-6">
-      <CookingStepsCards
-        instructions={recipe.instructions}
-        setTrackDoneSteps={() => setTrackDoneSteps(true)}
-      />
+      <CookingStepsCards instructions={recipe.instructions} setIsAllStepsDone={setIsAllStepsDone} />
 
       <div className="flex w-full justify-between pt-10">
         <button className="stepByStepBackBtn" onClick={() => onPrev()}>
           Back
         </button>
-        {isActive && trackDoneSteps && isLastStep && (
+        {isAllStepsDone && (
           <button className="stepByStepNextBtn" onClick={() => onNext()}>
             Done
           </button>
