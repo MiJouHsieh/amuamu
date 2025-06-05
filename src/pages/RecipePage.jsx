e9913a26c753a1c456c485b1e608d110d02478e3
MiJouHsieh <mijouhsieh@gmail.com>
2025-06-05 23:54:49 +0800

    fix(recipe): resolve merge conflict in RecipePage

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -6,13 +6,10 @@ import { MiniCartModal } from "src/components/MiniCartModal";
 import { CartIconToggle } from "src/components/CartIconToggle";
 import { RecipeImage } from "src/components/RecipeImage";
 import { SharedByUserLabel } from "src/components/recipe/SharedByUserLabel";
-<<<<<<< HEAD
 import { RecipeImageCarousel } from "src/components/recipe/RecipeImageCarousel";
 import { RecipeTags } from "src/components/recipe/RecipeTags";
 import { StepByStepBtn } from "src/components/recipe/StepByStepBtn";
-=======
 import { CookingTimer } from "src/components/recipe/CookingTimer";
->>>>>>> 0f40d81195e49f1e3bba9efb8a9df58bce5b838c
 
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
