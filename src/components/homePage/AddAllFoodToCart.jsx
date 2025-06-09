5c077b3aa0ea3d1bc2ee5f7ce5a032a7f0e343f9
MiJouHsieh <mijouhsieh@gmail.com>
2025-06-10 01:53:35 +0800

    style(addAllFoodToCart): remove redundant right margin for alignment

--- src/components/homePage/AddAllFoodToCart.jsx
+++ src/components/homePage/AddAllFoodToCart.jsx
@@ -5,7 +5,7 @@ export function AddAllFoodToCart({ onClick }) {
     <button
       type="button"
       onClick={onClick}
-      className="scale-1 group relative mr-6 transform rounded-full bg-beige p-3 transition-all hover:ring-2 hover:ring-orange/90 active:scale-[1.2]"
+      className="scale-1 group relative transform rounded-full bg-beige p-3 transition-all hover:ring-2 hover:ring-orange/90 active:scale-[1.2]"
     >
       <FiShoppingCart className="activeIcon h-6 w-6 text-orange900" />
       <span className="absolute -left-5 -top-12 scale-0 transform rounded bg-orange/90 px-2 py-1 text-xs text-blue800 transition duration-100 group-hover:scale-100">
